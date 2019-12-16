a8bd8d36645b50ebaa5264bfac7bea6978e446d1
Alessandro Butera <alexbu92@gmail.com>
2019-12-16 19:09:53 +0100

    added view3D button

--- wannatat-script.js
+++ wannatat-script.js
@@ -1,5 +1,17 @@
 window.onload = function () {  // make sure you run after the document has been loaded, otherwise you won't have access to jquery.
 	$( document ).ready(function() {
-    		$('#listing-image-link').css('background-color', 'white');
+            DisableImageBackground();
+            AddView3DButton();
 	});
+}
+
+function DisableImageBackground() {
+    $('#listing-image-link').css('background-color', 'white');
+}
+
+function AddView3DButton() {
+    var $src = $('#listing-image-link img').attr('src');
+    console.log($src);
+    var $input = $('<a href="https://www.thesitewizard.com/" target="_blank">thesitewizard.com</a>');
+
 }
