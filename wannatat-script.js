a9f225f5d23baea023877f1cd7a963ce0d3edb70
Alessandro Butera <alexbu92@gmail.com>
2019-12-16 12:04:22 +0100

    changed listing image bg color to white

--- wannatat-script.js
+++ wannatat-script.js
@@ -1,5 +1,5 @@
 window.onload = function () {  // make sure you run after the document has been loaded, otherwise you won't have access to jquery.
 	$( document ).ready(function() {
-    		console.log("ANTONTELLA");
+    		$('#listing-image-link').css('background-color', 'white');
 	});
 }
