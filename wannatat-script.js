3ff3d4853abdfa83f8711fe0462b18a204ef6c53
Alessandro Butera <alexbu92@gmail.com>
2019-12-16 11:55:19 +0100

    init test

---
+++ wannatat-script.js
@@ -0,0 +1,5 @@
+window.onload = function () {  // make sure you run after the document has been loaded, otherwise you won't have access to jquery.
+	$( document ).ready(function() {
+    		console.log("ANTONTELLA");
+	});
+}
