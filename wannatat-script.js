5cf48bcb08e20608eedb9b1a6938e7bb7a879263
Alessandro Butera <alexbu92@gmail.com>
2019-12-20 17:30:14 +0100

    added random uuid to image url

--- wannatat-script.js
+++ wannatat-script.js
@@ -14,11 +14,19 @@ function AddView3DButton() {
     var $src = $('#listing-image-link img').attr('src');
     $src = $src.replace(/\?(?:.(?!\?))+$/g, "");
     console.log($src);
-    var $input = $('<a href="https://wannatat.firebaseapp.com/?image=' + $src + '" target="_blank">View on 3D model</a>');
+    var uuid = uuidv4();
+    var $input = $('<a href="https://wannatat.firebaseapp.com/?image=' + $src + "?" + uuid + '" target="_blank">View on 3D model</a>');
     console.log($input);
     $('.listing-description-content').append($input);
 }
 
 function DisableQuantitySelector() {
     $('#quantity').attr("disabled", true);
+}
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
 }
